03103d9595014a63011101ec4481dcf6b8b143ac
tn841 <tn841@naver.com>
2020-07-22 11:11:05 +0900

    #4. Landing Page 만들기(1)

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+export const API_KEY = "5ea1e03ca603f6b0a011f71a944cc5a5";
+
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language-en-US&page=1`;
+
+        fetch(endpoint)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            setMovies([res.results]);
+            setMainMovieImage(res.results[0])
+        });
+    }, [])
+
     return (
-        <>
+        <div style={{ width: '100%', margin: '0'}}>
+            {MainMovieImage && //MainMovieImage State가 설정되면, 아래 Component를 랜더링 한다.
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
             <div className="app">
                 <FaCode style={{ fontSize: '4rem' }} /><br />
                 <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
             </div>
             <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        </div>
     )
 }
 
